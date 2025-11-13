e7fe0d44fa8a25386d96681c4ef2f4fe8f1d6e8a
Samuel Luna <samuhluna@gmail.com>
2025-11-13 00:01:57 -0300

    initialize 'mock' implemention for CreateUseCase

--- tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
+++ tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -1,4 +1,6 @@
 ﻿using CashFlow.Application.UseCase.Users.Register;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
 
@@ -21,6 +23,10 @@ public class RegisterUserUseCaseTest
 
     private RegisterUserUseCase CreateUseCase()
     {
-        return new RegisterUserUseCase();
+        var mapper = MapperBuilder.Build();
+        var unitOfWork  = UnitOfWorkBuilder.Build();
+        var writeOnlyRepository = UserWriteOnlyRepositoryBuilder.Build();
+
+        return new RegisterUserUseCase(mapper, null, null, writeOnlyRepository, unitOfWork, null);
     }
 }
